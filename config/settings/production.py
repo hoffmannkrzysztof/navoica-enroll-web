1838a58fcaa3af4bcb26c197fdff8e7c4b9768f7
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2020-02-13 12:26:54 +0100

    change production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -86,14 +86,14 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL", default="{} <noreply@{}>".format(
-        _("Navoica Remote Enrollment for a Course"), env("DOMAIN"))
+        "Navoica Remote Enrollment for a Course", env("DOMAIN"))
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
     "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[{}]".format(_("Navoica Remote Enrollment for a Course"))
+    default="[{}]".format("Navoica Remote Enrollment for a Course")
 )
 
 # ADMIN
