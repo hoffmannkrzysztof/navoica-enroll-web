5d0f319aeba1e7625b1b1b5385742cb8f0cc9565
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2020-04-21 11:31:25 +0200

    set example domain name for site

--- navoica_enroll/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ navoica_enroll/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -15,7 +15,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "",
+            "domain": "change_me.com",
             "name": "Navoica Remote Enrollment for a Course",
         },
     )
